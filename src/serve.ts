19caf883f89cf5de66a044dec03d4a3d0db9562a
Denis Moreira <silvad281@gmail.com>
2024-05-14 16:42:53 -0300

    feat: config do server

--- src/serve.ts
+++ src/serve.ts
@@ -1,12 +1,24 @@
+require("dotenv").config();
+
 import express from "express";
 import config from "config";
+import router from "./router";
+import db from "../config/db";
+import Logger from "../config/logger";
+import morganMiddleware from "./middlewares/morganMiddleware";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api/", router);
+
+app.use(morganMiddleware);
+
 const port = config.get<number>("port");
 
 app.listen(3000, async () => {
-   console.log(`Serve online da porta: ${port}`);
+   await db();
+
+   Logger.info(`Serve online da porta: ${port}`);
 });
