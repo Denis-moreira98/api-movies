3034965099cdf60d2f1fff69ad95682e3edd4aa9
Denis Moreira <silvad281@gmail.com>
2024-05-15 14:15:12 -0300

    feat: rota para buscar filme por id

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 
 //controllers
 import CreateMovieController from "./controllers/CreateMovieController";
+import FindMovieByIdController from "./controllers/FindMovieByIdController";
 
 // Middlewares
-import { validate } from "./middlewares/handlevalidation";
+import { validate } from "./middlewares/handleValidation";
 import { movieCreateValidation } from "./middlewares/movieValidation";
 
 const router = Router();
@@ -15,5 +16,6 @@ router.post(
    validate,
    new CreateMovieController().creteMovie
 );
+router.get("/movie/:id", new FindMovieByIdController().findMovieById);
 
 export { router };
